0448971e53621f2f0dffc6facfa3cb868a4e0a42
David Alexander Pfeiffer <david.pfeiffer@wildcodeschool.com>
2023-09-18 15:17:12 +0200

    Fix DoctorController.java
    Removed trailing slash on route which breaks newer versions of Spring

--- src/main/java/com/example/thymeleaf/controller/DoctorController.java
+++ src/main/java/com/example/thymeleaf/controller/DoctorController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor/")
+    @GetMapping("/doctor")
     public String doctor() {
 
         return "doctor";
